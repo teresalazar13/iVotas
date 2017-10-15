7bed4323676450b7f61cb706f8c74c52da306ffe
Teresa Margarida Baptista Cabral De Carvalho Salazar <tmsalazar@student.dei.uc.pt>
2017-10-15 16:14:55 +0100

    Fixes indentation.

--- src/Data/File.java
+++ src/Data/File.java
@@ -3,35 +3,35 @@ package Data;
 import java.io.*;
 
 public class File {
-    private ObjectInputStream ois;
-    private ObjectOutputStream oos;
+  private ObjectInputStream ois;
+  private ObjectOutputStream oos;
 
-    public boolean openRead(String nomeDoFicheiro){
-        try {
-            ois = new ObjectInputStream(new FileInputStream(nomeDoFicheiro));
-            return true;
-        } catch (IOException e) {
-            return false;
-        }
+  public boolean openRead(String filename){
+    try {
+      ois = new ObjectInputStream(new FileInputStream(filename));
+      return true;
+    } catch (IOException e) {
+      return false;
     }
+  }
 
-    public void openWrite(String nomeDoFicheiro) throws IOException {
-        oos = new ObjectOutputStream(new FileOutputStream(nomeDoFicheiro));
-    }
+  public void openWrite(String filename) throws IOException {
+    oos = new ObjectOutputStream(new FileOutputStream(filename));
+  }
 
-    public void writeObject(Object o) throws IOException {
-        oos.writeObject(o);
-    }
+  public void writeObject(Object o) throws IOException {
+    oos.writeObject(o);
+  }
 
-    public Object readObject() throws IOException, ClassNotFoundException {
-        return ois.readObject();
-    }
+  public Object readObject() throws IOException, ClassNotFoundException {
+    return ois.readObject();
+  }
 
-    public void closeRead() throws  IOException {
-        ois.close();
-    }
+  public void closeRead() throws  IOException {
+    ois.close();
+  }
 
-    public void closeWrite() throws IOException {
-        oos.close();
-    }
+  public void closeWrite() throws IOException {
+    oos.close();
+  }
 }
--- src/Data/User.java
+++ src/Data/User.java
@@ -3,98 +3,97 @@ import java.io.Serializable;
 
 
 public class User implements Serializable {
-    private String name;
-    private String password;
-    private Department department;
-    private Faculty faculty;
-    private String contact;
-    private String address;
-    private String cc;
-    private String expireDate;
-    private int type;
+  private String name;
+  private String password;
+  private Department department;
+  private Faculty faculty;
+  private String contact;
+  private String address;
+  private String cc;
+  private String expireDate;
+  private int type;
 
-    public User() {}
+  public User() {}
 
-    public User(String name, String password, Department department, Faculty faculty, String contact, String address, String cc, String expireDate, int type) {
-        this.name = name;
-        this.password = password;
-        this.department = department;
-        this.faculty = faculty;
-        this.contact = contact;
-        this.address = address;
-        this.cc = cc;
-        this.expireDate = expireDate;
-        this.type = type;
-    }
+  public User(String name, String password, Department department, Faculty faculty, String contact, String address, String cc, String expireDate, int type) {
+    this.name = name;
+    this.password = password;
+    this.department = department;
+    this.faculty = faculty;
+    this.contact = contact;
+    this.address = address;
+    this.cc = cc;
+    this.expireDate = expireDate;
+    this.type = type;
+  }
 
-    // Getters and Setters
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
+  public String getPassword() {
+    return password;
+  }
+  public void setPassword(String password) {
+    this.password = password;
+  }
 
-    public Department getDepartment() { return department; }
-    public void setDepartment(Department department) { this.department = department; }
+  public Department getDepartment() { return department; }
+  public void setDepartment(Department department) { this.department = department; }
 
-    public Faculty getFaculty() { return faculty; }
-    public void setFaculty(Faculty faculty) { this.faculty = faculty; }
+  public Faculty getFaculty() { return faculty; }
+  public void setFaculty(Faculty faculty) { this.faculty = faculty; }
 
-    public String getContact() {
-        return contact;
-    }
-    public void setContact(String contact) {
-        this.contact = contact;
-    }
+  public String getContact() {
+    return contact;
+  }
+  public void setContact(String contact) {
+    this.contact = contact;
+  }
 
-    public String getAddress() {
-        return address;
-    }
-    public void setAddress(String address) {
-        this.address = address;
-    }
+  public String getAddress() {
+    return address;
+  }
+  public void setAddress(String address) {
+    this.address = address;
+  }
 
-    public String getCc() {
-        return cc;
-    }
-    public void setCc(String cc) {
-        this.cc = cc;
-    }
+  public String getCc() {
+    return cc;
+  }
+  public void setCc(String cc) {
+    this.cc = cc;
+  }
 
-    public String getExpireDate() {
-        return expireDate;
-    }
-    public void setExpireDate(String expireDate) {
-        this.expireDate = expireDate;
-    }
+  public String getExpireDate() {
+    return expireDate;
+  }
+  public void setExpireDate(String expireDate) {
+    this.expireDate = expireDate;
+  }
 
-    public int getType() {
-        return type;
-    }
-    public void setType(int type) {
-        this.type = type;
-    }
+  public int getType() {
+    return type;
+  }
+  public void setType(int type) {
+    this.type = type;
+  }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                ", department=" + department +
-                ", faculty=" + faculty +
-                ", contact='" + contact + '\'' +
-                ", address='" + address + '\'' +
-                ", cc='" + cc + '\'' +
-                ", expireDate='" + expireDate + '\'' +
-                ", type=" + type +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "User{" +
+            "name='" + name + '\'' +
+            ", password='" + password + '\'' +
+            ", department=" + department +
+            ", faculty=" + faculty +
+            ", contact='" + contact + '\'' +
+            ", address='" + address + '\'' +
+            ", cc='" + cc + '\'' +
+            ", expireDate='" + expireDate + '\'' +
+            ", type=" + type +
+            '}';
+  }
 }
