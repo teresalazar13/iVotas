e35e397000b5b3e4943d38dd38a0183ac34e97c7
Kabombom <mantonio@student.dei.uc.pt>
2017-10-15 16:31:05 +0100

    Fixes getters and setters

--- src/Data/User.java
+++ src/Data/User.java
@@ -27,19 +27,11 @@ public class User implements Serializable {
     this.type = type;
   }
 
-  public String getName() {
-    return name;
-  }
-  public void setName(String name) {
-    this.name = name;
-  }
+  public String getName() { return name; }
+  public void setName(String name) { this.name = name; }
 
-  public String getPassword() {
-    return password;
-  }
-  public void setPassword(String password) {
-    this.password = password;
-  }
+  public String getPassword() { return password; }
+  public void setPassword(String password) { this.password = password; }
 
   public Department getDepartment() { return department; }
   public void setDepartment(Department department) { this.department = department; }
@@ -47,40 +39,20 @@ public class User implements Serializable {
   public Faculty getFaculty() { return faculty; }
   public void setFaculty(Faculty faculty) { this.faculty = faculty; }
 
-  public String getContact() {
-    return contact;
-  }
-  public void setContact(String contact) {
-    this.contact = contact;
-  }
+  public String getContact() { return contact; }
+  public void setContact(String contact) { this.contact = contact; }
 
-  public String getAddress() {
-    return address;
-  }
-  public void setAddress(String address) {
-    this.address = address;
-  }
+  public String getAddress() { return address; }
+  public void setAddress(String address) { this.address = address; }
 
-  public String getCc() {
-    return cc;
-  }
-  public void setCc(String cc) {
-    this.cc = cc;
-  }
+  public String getCc() { return cc; }
+  public void setCc(String cc) { this.cc = cc; }
 
-  public String getExpireDate() {
-    return expireDate;
-  }
-  public void setExpireDate(String expireDate) {
-    this.expireDate = expireDate;
-  }
+  public String getExpireDate() { return expireDate; }
+  public void setExpireDate(String expireDate) { this.expireDate = expireDate; }
 
-  public int getType() {
-    return type;
-  }
-  public void setType(int type) {
-    this.type = type;
-  }
+  public int getType() { return type; }
+  public void setType(int type) { this.type = type; }
 
   @Override
   public String toString() {
