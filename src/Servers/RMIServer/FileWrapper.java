d3aa73dc44e339c9f0f9e144bda341feafc0fd9e
Kabombom <mantonio@student.dei.uc.pt>
2017-10-19 16:31:10 +0100

    Adds more departments in FileWrapper

--- src/Servers/RMIServer/FileWrapper.java
+++ src/Servers/RMIServer/FileWrapper.java
@@ -7,70 +7,74 @@ import java.util.ArrayList;
 
 public class FileWrapper {
 
-    ArrayList<User> users;
-    ArrayList<Faculty> faculties;
-    ArrayList<Department> departments;
-    ArrayList<Election> elections;
-    ArrayList<CandidateList> candidateLists;
-    ArrayList<VotingTable> votingTables;
+  ArrayList<User> users;
+  ArrayList<Faculty> faculties;
+  ArrayList<Department> departments;
+  ArrayList<Election> elections;
+  ArrayList<CandidateList> candidateLists;
+  ArrayList<VotingTable> votingTables;
 
-    public FileWrapper() throws IOException, ClassNotFoundException {
-        Department department1 = new Department("EngenhariaInformatica");
-        ArrayList<Department> departments = new ArrayList<Department>();
-        departments.add(department1);
+  public FileWrapper() throws IOException, ClassNotFoundException {
+    Department department1 = new Department("EngenhariaInformatica");
+    Department department2 = new Department("EngenhariaMecanica");
+    Department department3 = new Department("EngenhariaEletro");
+    ArrayList<Department> departments = new ArrayList<Department>();
+    departments.add(department1);
+    departments.add(department2);
+    departments.add(department3);
 
-        Faculty faculty1 = new Faculty("FCTUC", departments);
-        ArrayList<Faculty> faculties = new ArrayList<Faculty>();
-        faculties.add(faculty1);
+    Faculty faculty1 = new Faculty("FCTUC", departments);
+    ArrayList<Faculty> faculties = new ArrayList<Faculty>();
+    faculties.add(faculty1);
 
-        User user1= new User("Teresa", "123", department1, faculty1, "9140975", "Rua X", "444",
-                "expireDate", 1 );
-        ArrayList<User> users = new ArrayList<User>();
-        users.add(user1);
+    User user1= new User("Teresa", "123", department1, faculty1, "9140975", "Rua X", "444",
+            "expireDate", 1 );
+    ArrayList<User> users = new ArrayList<User>();
+    users.add(user1);
 
-        writeFile(users,"Users");
-        ArrayList<User> usersFromFile = (ArrayList<User>) readFile("User");
+    writeFile(users,"Users");
+    ArrayList<User> usersFromFile = (ArrayList<User>) readFile("User");
 
-        writeFile(departments,"Departments");
-        ArrayList<Department> departmentsFromFile = (ArrayList<Department>) readFile("Department");
+    writeFile(departments,"Departments");
+    ArrayList<Department> departmentsFromFile = (ArrayList<Department>) readFile("Department");
 
-        writeFile(faculties,"Faculties");
-        ArrayList<Faculty> facultiesFromFile = (ArrayList<Faculty>) readFile("Faculty");
+    writeFile(faculties,"Faculties");
+    ArrayList<Faculty> facultiesFromFile = (ArrayList<Faculty>) readFile("Faculty");
 
-        this.users = usersFromFile;
-        this.departments = departmentsFromFile;
-        this.faculties = facultiesFromFile;
+    this.users = usersFromFile;
+    this.departments = departmentsFromFile;
+    this.faculties = facultiesFromFile;
+  }
+
+  private static void writeFile(Object classe, String className) throws IOException, ClassNotFoundException {
+    String filename = "ObjectFiles/" + className + ".dat";
+    File file = new File();
+    file.openWrite(filename);
+    file.writeObject(classe);
+    file.closeWrite();
+  }
+
+  private static ArrayList readFile(String className) throws IOException, ClassNotFoundException {
+    String filename;
+    if (className.substring(className.length() - 1).equals("y")) {
+      filename = "ObjectFiles/" + className.substring(0, className.length() - 1) + "ies" + ".dat";
     }
-
-    private static void writeFile(Object classe, String className) throws IOException, ClassNotFoundException {
-        String filename = "ObjectFiles/" + className + ".dat";
-        File file = new File();
-        file.openWrite(filename);
-        file.writeObject(classe);
-        file.closeWrite();
+    else {
+      filename = "ObjectFiles/" + className + "s" + ".dat";
     }
-
-    private static ArrayList readFile(String className) throws IOException, ClassNotFoundException {
-        String filename;
-        if (className.substring(className.length() - 1).equals("y")) {
-            filename = "ObjectFiles/" + className.substring(0, className.length() - 1) + "ies" + ".dat";
-        }
-        else {
-            filename = "ObjectFiles/" + className + "s" + ".dat";
-        }
-        File file = new File();
-        file.openRead(filename);
-        ArrayList <Object> array = null;
-        try {
-            array = (ArrayList <Object>) file.readObject();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        file.closeRead();
-        return array;
+    File file = new File();
+    file.openRead(filename);
+    ArrayList <Object> array = null;
+    try {
+      array = (ArrayList <Object>) file.readObject();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
     }
+    file.closeRead();
+    return array;
+  }
 
 
 }
